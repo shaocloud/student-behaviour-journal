3faecf1940d9f451ef5c1716393584aa78b742a0
Shao Yun TAN <shao.cloud@gmail.com>
2026-05-04 11:36:00 +0800

    style(fe): adjust student_profile

--- frontend/src/pages/student_profile.tsx
+++ frontend/src/pages/student_profile.tsx
@@ -43,33 +43,30 @@ export default function StudentProfile()
     const student = useLoaderData() as Student;
 
     return(
-        <div>
-        <Card className="max-w-lg py-0 sm:flex-row sm:gap-0">
-            <CardContent className='grow-1 px-0'>
-                <img
-                src='https://cdn.shadcnstudio.com/ss-assets/components/card/image-3.png'
-                alt='Banner'
-                className='size-full rounded-l-xl'
-                />
-            </CardContent>
-            <div className='py-10 sm:min-w-54'>          
-            <CardHeader>
-                <CardTitle>{student.name}, {student.age}</CardTitle>
-                <CardDescription>
-                    Student ID: {student.id}
-                </CardDescription>
-            </CardHeader>
-            <CardContent className='py-2'>
-                <p>Grade: {student.grade}</p>
-                <p>Behaviour History: {student.behaviourHistory}</p>
-            </CardContent>
-            </div>
-            <CardFooter>
-                <p>Card footer</p>
-            </CardFooter>
-        </Card>
+        <div className="flex flex-col md:flex-row gap-4">
+            <Card className="max-w-lg py-0 md:flex-row md:gap-0">
+                <CardContent className='grow-1 px-0'>
+                    <img
+                    src='https://cdn.shadcnstudio.com/ss-assets/components/card/image-3.png'
+                    alt='Banner'
+                    className='size-full rounded-l-xl'
+                    />
+                </CardContent>
+                <div className='py-10 sm:min-w-54'>          
+                <CardHeader>
+                    <CardTitle>{student.name}, {student.age}</CardTitle>
+                    <CardDescription>
+                        Student ID: {student.id}
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className='py-2'>
+                    <p>Grade: {student.grade}</p>
+                    <p>Behaviour History: {student.behaviourHistory}</p>
+                </CardContent>
+                </div>
+            </Card>
 
-        <BehTable />
+            <BehTable />
         </div>
     )
 }
